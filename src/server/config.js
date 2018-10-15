2b486a08d594acf44d99fa2395925da7972101c8
Christian Wickerström <christianwickerstrom@Christians-MacBook-Pro.local>
2018-10-15 15:19:16 +0200

    config update

--- src/server/config.js
+++ src/server/config.js
@@ -1,2 +1,3 @@
-module.exports = { Authorization: 'VUFmVExKQ1pmM1JLdDNma3NUc3ZudWRnNnFJYTpDSEIwU01Qal8wMGFPNGVCbDNZUk85ODlvcEFh' };
+/* Go to https://developer.vasttrafik.se/portal/ and register and use your own credentials! */
+module.exports = { Authorization: '[YOUR CREDENTIALS HERE!]' };
 
