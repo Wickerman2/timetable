e67854be27ecb617e07639909ee5ecd049e2c297
Christian Wickerström <christianwickerstrom@Christians-MacBook-Pro.local>
2018-10-15 15:26:05 +0200

    gitignore fix

--- src/server/config.js
+++ src/server/config.js
@@ -1,3 +1,3 @@
 /* Go to https://developer.vasttrafik.se/portal/ and register and use your own credentials! */
-module.exports = { Authorization: '[Your credentials here!]' };
+module.exports = { Authorization: '[Your credentials here! !!! ]' };
 
