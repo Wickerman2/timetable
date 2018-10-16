6cb47f2e9aad357fe502ac5b2157ef9cd777ce9d
Christian Wickerström <christianwickerstrom@Christians-MacBook-Pro.local>
2018-10-16 14:17:30 +0200

    npm start fix

--- src/server/config.js
+++ src/server/config.js
@@ -1,2 +1,5 @@
 /* Go to https://developer.vasttrafik.se/portal/ and register and use your own credentials! */
 module.exports = { Authorization: 'VUFmVExKQ1pmM1JLdDNma3NUc3ZudWRnNnFJYTpDSEIwU01Qal8wMGFPNGVCbDNZUk85ODlvcEFh' };
+
+
+// "npm-run-all -p watch-css start-js",
